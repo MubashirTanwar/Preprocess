15b2781265f1623c9e03902fc7b832cc94941877
MubashirTanwar <119968206+MubashirTanwar@users.noreply.github.com>
2024-01-04 13:40:39 +0530

    Update prepo.py

--- prepo.py
+++ prepo.py
@@ -1,87 +1,50 @@
-import cv2
-import numpy as np
-from IPython.display import Image
 import sys
+import numpy as np
+import cv2
+import os
+
+
+def get_string(img_path):
+    
+    img = cv2.imread(img_path)
+
+    file_name = os.path.basename(img_path).split('.')[0]
+    file_name = file_name.split()[0]
+
+    output_path = os.path.join('output_path', "ocr")
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    img = cv2.resize(img, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_CUBIC)
+
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    rgb_planes = cv2.split(img)
+    result_planes = []
+    for plane in rgb_planes:
+        dilated_img = cv2.dilate(plane, np.ones((7,7), np.uint8))
+        bg_img = cv2.medianBlur(dilated_img, 21)
+        diff_img = 255 - cv2.absdiff(plane, bg_img)
+        result_planes.append(diff_img)
+    img = cv2.merge(result_planes)
+    
+
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1) 
+    
+
+    img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+    
+
+    save_path = os.path.join(output_path, file_name + "_filter_" + str('as') + ".png")
+    cv2.imwrite(save_path, img)
+    
+    
+    return save_path
 
 if len(sys.argv) != 2:
-    sys.exit("Usage: python prepo.py filename")
+    sys.exit("Usage: python ocr.py filename")
 
+image_file = sys.argv[1]
 
-    
-
-def preprocess_image(image_path):
-    # Read the original image
-    original_img = cv2.imread(image_path, cv2.IMREAD_COLOR)
-
-    # Convert to grayscale
-    gray = cv2.cvtColor(original_img, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)
-
-    # Apply thresholding
-    _, binary = cv2.threshold(blur, 160, 220, cv2.THRESH_BINARY)
-
-    #remove borders
-    
-    # Noise reduction
-    # denoised = noise_removal(binary)
-
-    #Deskew Image
-    def deskew(cvImage):
-        angle = getSkewAngle(cvImage)
-        return rotateImage(cvImage, -1.0 * angle)
-    fixed = deskew(binary)
-    no_borders = remove_borders(fixed)
-    
-    return no_borders
-
-def noise_removal(image):
-    kernel = np.ones((1, 1), np.uint8)
-    image = cv2.dilate(image, kernel, iterations=1)
-    image = cv2.GaussianBlur(image, (5,5),0)
-    return (image)
-
-def getSkewAngle(cvImage):
-    # Prep image, copy, convert to gray scale, blur, and threshold
-    newImage = cvImage.copy()
-
-    # Find all contours
-    contours, hierarchy = cv2.findContours(newImage, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    contours = sorted(contours, key = cv2.contourArea, reverse = True)
-    for c in contours:
-        rect = cv2.boundingRect(c)
-        x,y,w,h = rect
-        cv2.rectangle(newImage,(x,y),(x+w,y+h),(0,255,0),2)
-
-    # Find largest contour and surround in min area box
-    largestContour = contours[0]
-    print (len(contours))
-    minAreaRect = cv2.minAreaRect(largestContour)
-    cv2.imwrite("temp/boxes.jpg", newImage)
-    # Determine the angle. Convert it to the value that was originally used to obtain skewed image
-    angle = minAreaRect[-1]
-    print(angle)
-    if angle < -45:
-        angle = 90 + angle
-    return -1.0 * angle
-# Rotate the image around its center
-def rotateImage(cvImage, angle: float):
-    newImage = cvImage.copy()
-    (h, w) = newImage.shape[:2]
-    center = (w // 2, h // 2)
-    M = cv2.getRotationMatrix2D(center, angle, 1.0)
-    newImage = cv2.warpAffine(newImage, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
-    return newImage
-# Deskew image
-
-def remove_borders(image):
-    contours, heiarchy = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cntsSorted = sorted(contours, key=lambda x:cv2.contourArea(x))
-    cnt = cntsSorted[-1]
-    x, y, w, h = cv2.boundingRect(cnt)
-    crop = image[y:y+h, x:x+w]
-    return (crop)
-#++++++++++++++++++++++++++++++++++++++++++++++++++
-
-image = sys.argv[1]
-result = preprocess_image(image)
-cv2.imwrite("preprocessed_image9.png", result)
+get_string(image_file)
